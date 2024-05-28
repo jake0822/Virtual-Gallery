ef9a7508155897903586a1cc745dd3d93f40efd3
jacob <jakeyounan16@gmail.com>
2024-05-28 10:30:17 -0400

    creates item

---
+++ Virtual Gallery/Library.VirtualGallery/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿namespace Library.VirtualGallery.Models
+{
+    public class Item
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? ARLink { get; set; }
+        public int Id { get; set; }
+        public int YearMade { get; set; }
+
+        public Item()
+        {
+            Title = string.Empty;
+            Description = string.Empty;
+            ARLink = string.Empty;
+            YearMade = 0;
+            Id = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Id}] {Title} - Made in {YearMade} - {Description} - AR Experience: {ARLink}";
+        }
+
+    }
+}
